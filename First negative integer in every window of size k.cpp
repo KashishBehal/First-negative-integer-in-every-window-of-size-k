0fa3bea7b2509539be70d0e7fea745a77923eac8
Kashish Behal <113112253+KashishBehal@users.noreply.github.com>
2023-12-13 01:24:29 +0530

    Create First negative integer in every window of size k.cpp

---
+++ First negative integer in every window of size k.cpp
@@ -0,0 +1,25 @@
+
+vector<long long> printFirstNegativeInteger(long long int A[],
+                                             long long int N, long long int K) {
+                              
+                              
+                             
+    vector<long long>v;
+    queue<long long >q;
+    int i=0 , j=0;
+    while(j<N){
+        if(A[j]<0){
+            q.push(A[j]);
+        }
+        if(j-i+1==K){
+            if(!q.empty()){
+                v.push_back(q.front());
+                if(A[i]<0)q.pop();
+            }
+            else v.push_back(0);
+            i++;
+        }
+        j++;
+    }
+    return v;
+ }
